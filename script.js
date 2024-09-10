1614912015327b0ef4b550dfd680aa8532342baf
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-09-10 12:12:50 -0700

    Solved part 1

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ If a number is divisible by 3, log “Fizz.”
 If a number is divisible by 5, log “Buzz.”
 If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 If a number is not divisible by either 3 or 5, log the number.*/
+
+let fbNum = 1;
+while (fbNum <= 100) {
+    if (fbNum % 3 == 0 && fbNum % 5 == 0) {
+        console.log(`Fizz Buzz`);
+    } else if (fbNum % 3 == 0) {
+        console.log(`Fizz`);
+    } else if (fbNum % 5 == 0) {
+        console.log(`Buzz`);
+    } else {
+        console.log(fbNum);
+    }
+    fbNum ++;
+}
